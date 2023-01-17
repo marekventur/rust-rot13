a06cb22268b1519701d43de070fc39ba804878c4
Adrian Groh <adrian.groh@t-online.de>
2023-01-17 16:17:38 +0100

    Use new inclusive range syntax

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,8 @@
 pub fn rot13(text: &str) -> String {
     text.chars().map(|c| {
         match c {
-            'A' ... 'M' | 'a' ... 'm' => ((c as u8) + 13) as char,
-            'N' ... 'Z' | 'n' ... 'z' => ((c as u8) - 13) as char,
+            'A' ..= 'M' | 'a' ..= 'm' => ((c as u8) + 13) as char,
+            'N' ..= 'Z' | 'n' ..= 'z' => ((c as u8) - 13) as char,
             _ => c
         }
     }).collect()
